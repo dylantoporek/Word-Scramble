f39a67e89cf930e7c2f94c407fc2799171b7c89e
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-07-31 11:12:07 -0400

    scramble function added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,35 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
+
+//   const word = 'abandon'
+// let wordArr = word.split('')
+
+
+// function shuffle(array) {
+//   let currentIndex = array.length, randomIndex;
+//   console.log(currentIndex)
+//   // While there remain elements to shuffle.
+//   while (currentIndex != 0) {
+
+//     // Pick a remaining element.
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex--;
+
+//     // And swap it with the current element.
+//     [array[currentIndex], array[randomIndex]] = [
+//       array[randomIndex], array[currentIndex]];
+//   }
+
+//   return array;
+// }
+
+// // Used like so
+// var arr = [2, 11, 37, 42];
+// shuffle(wordArr);
+// console.log(wordArr);
+
+
   return (
     <div className="App">
       <header className="App-header">
