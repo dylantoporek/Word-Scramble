989b90a33093aaaba899b56518f2615b93dd2727
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-08-06 14:54:02 -0400

    settings style updates

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -7,20 +7,21 @@ import '../Settings/index.scss'
 
 function Settings({handleClosePopUp}){
     const dispatch = useDispatch()
-    // function handleClick(){
-    //     setToggleNavPop(false)
-    // }
+    
    
     return (
         <div className='settings-container'>
             <div className='pop-up-title'>
-                <h2>Settings</h2>
+                <h3>SETTINGS</h3>
                 <button onClick={(e)=> handleClosePopUp(e)} className='close'>X</button>
             </div>
             
-            <div>
-                <span>dark mode</span>
-                <button onClick={()=> dispatch(toggle())}>toggle</button>
+            <div className='settings'>
+                <div>
+                    <span>DARK MODE</span>
+                    <button onClick={()=> dispatch(toggle())}>toggle</button>
+                </div>
+                
             </div>
             
 
