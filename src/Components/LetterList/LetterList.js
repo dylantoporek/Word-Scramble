cb07fd6175e8237b0b125a73e6a31aaa1f08d595
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-08-22 07:02:30 -0400

    words forming

--- src/Components/LetterList/LetterList.js
+++ src/Components/LetterList/LetterList.js
@@ -4,41 +4,54 @@ import {useDispatch, useSelector} from 'react-redux'
 import {selectWord1, selectGameOver, selectGuessCount, formWord1} from '../../app/Redux Slices/gameSlice'
 
 function LetterList({scramble, pointsObj}){
-const dispatch  = useDispatch()
+    const [alreadyUsed, setAlreadyUsed] = useState([])
+    const [errors, setErrors] = useState({
+        message: '',
+        show: false,
+    })
+
+    const dispatch  = useDispatch()
 
 
 
-    function handleLetterClick(e){
-       let chosenIndex = e.target.querySelector('span').id
 
-        if(e.target.innerText.length > 1){
-            let picked = e.target.innerText[0]
-            checkIfAvailable(picked, chosenIndex)
-            // dispatch(formWord1(picked))
-        } else {
-            let picked = e.target.innerText
-            checkIfAvailable(picked, chosenIndex)
-            // dispatch(formWord1(picked))
-        }
-        
+    function errorCleanup(){
+        setTimeout(()=>{
+            setErrors({
+                message: '',
+                show: false
+            })
+        }, 2000)
+            
     }
-
-    function checkIfAvailable(letter, index){
-        
-        console.log(index)
-        console.log(letter)
+    function handleLetterClick(e){
+        if (alreadyUsed.includes(e.target.id)){
+            setErrors({
+                message: 'Letter already used in this word.',
+                show: true
+            })
+            setTimeout((errorCleanup(), 1000))
+        } else {
+            setAlreadyUsed([...alreadyUsed, e.target.id])
+            dispatch(formWord1(e.target.id[0]))
+        }
     }
 
     let scrambleDisplay = scramble.map((letter, index) => {
-        return <div key={`${letter}-${index}`} className='letter' onClick={handleLetterClick}>
-            <span id={index} className='l'>{letter}</span>
-            <p className='score'>{pointsObj[letter]}</p>
+        return <div key={`${letter}-${index}`} id={`${letter + index}`} className='letter' onClick={handleLetterClick}>
+            <span id={`${letter + index}`} className='l'>{letter}</span>
+            <p id={`${letter + index}`} className='score'>{pointsObj[letter]}</p>
         </div>
     })
+
+    const errorsDisplay = <div>
+        {errors.message}
+    </div>
 
     return (
     <div className='letters'>
         {scrambleDisplay}
+        {errors.show ? errorsDisplay : null}
     </div>
 )
 }
--- src/Components/Word/Word.js
+++ src/Components/Word/Word.js
@@ -6,54 +6,54 @@ import {selectWord1, selectGameOver, selectGuessCount} from '../../app/Redux Sli
 
 function Word(){
     const dispatch = useDispatch()
-    console.log(useSelector(selectWord1))
+    let word1 = (useSelector(selectWord1))
     return (
         <div className='words-container'>
-            <form>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-            </form>
-            <form>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-            </form>
-            <form>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-            </form>
-            <form>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-            </form>
-            <form>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
-            </form>
+            <div>
+                <span>{word1.letterArr[0]? word1.letterArr[0] : null}</span>
+                <span>{word1.letterArr[1]? word1.letterArr[1] : null}</span>
+                <span>{word1.letterArr[2]? word1.letterArr[2] : null}</span>
+                <span>{word1.letterArr[3]? word1.letterArr[3] : null}</span>
+                <span>{word1.letterArr[4]? word1.letterArr[4] : null}</span>
+                <span>{word1.letterArr[5]? word1.letterArr[5] : null}</span>
+                <span>{word1.letterArr[6]? word1.letterArr[6] : null}</span>
+            </div>
+            {/* <div>
+                <span>{word1.letterArr[0]? word1.letterArr[0] : null}</span>
+                <span>{word1.letterArr[1]? word1.letterArr[1] : null}</span>
+                <span>{word1.letterArr[2]? word1.letterArr[2] : null}</span>
+                <span>{word1.letterArr[3]? word1.letterArr[3] : null}</span>
+                <span>{word1.letterArr[4]? word1.letterArr[4] : null}</span>
+                <span>{word1.letterArr[5]? word1.letterArr[5] : null}</span>
+                <span>{word1.letterArr[6]? word1.letterArr[6] : null}</span>
+            </div>
+            <div>
+                <span>{word1.letterArr[0]? word1.letterArr[0] : null}</span>
+                <span>{word1.letterArr[1]? word1.letterArr[1] : null}</span>
+                <span>{word1.letterArr[2]? word1.letterArr[2] : null}</span>
+                <span>{word1.letterArr[3]? word1.letterArr[3] : null}</span>
+                <span>{word1.letterArr[4]? word1.letterArr[4] : null}</span>
+                <span>{word1.letterArr[5]? word1.letterArr[5] : null}</span>
+                <span>{word1.letterArr[6]? word1.letterArr[6] : null}</span>
+            </div>
+            <div>
+                <span>{word1.letterArr[0]? word1.letterArr[0] : null}</span>
+                <span>{word1.letterArr[1]? word1.letterArr[1] : null}</span>
+                <span>{word1.letterArr[2]? word1.letterArr[2] : null}</span>
+                <span>{word1.letterArr[3]? word1.letterArr[3] : null}</span>
+                <span>{word1.letterArr[4]? word1.letterArr[4] : null}</span>
+                <span>{word1.letterArr[5]? word1.letterArr[5] : null}</span>
+                <span>{word1.letterArr[6]? word1.letterArr[6] : null}</span>
+            </div>
+            <div>
+                <span>{word1.letterArr[0]? word1.letterArr[0] : null}</span>
+                <span>{word1.letterArr[1]? word1.letterArr[1] : null}</span>
+                <span>{word1.letterArr[2]? word1.letterArr[2] : null}</span>
+                <span>{word1.letterArr[3]? word1.letterArr[3] : null}</span>
+                <span>{word1.letterArr[4]? word1.letterArr[4] : null}</span>
+                <span>{word1.letterArr[5]? word1.letterArr[5] : null}</span>
+                <span>{word1.letterArr[6]? word1.letterArr[6] : null}</span>
+            </div> */}
         </div>
     )
 }
