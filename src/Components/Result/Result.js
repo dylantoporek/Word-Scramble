8a359a3dc5b8658b1fb426d7673a016acff658bf
RaccoonNW <87783832+RaccoonNW@users.noreply.github.com>
2022-08-26 12:42:29 -0600

    Sorting definitions by partOfSpeech

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -3,13 +3,52 @@ import '../Result/index.scss'
 function Result({props}){
 
     console.log(props)
+    const definitionsNouns = props.response.results.map((word) => {
+        if(word.partOfSpeech === 'noun') {
+            return (
+                <li>
+                    {word.definition}
+                </li>
+            )
+        }
+    })
+
+    const definitionsVerb = props.response.results.map((word) => {
+        if(word.partOfSpeech === 'verb') {
+            return (
+                <li>
+                    {word.definition}
+                </li>
+            )
+        }
+    })
+
+    console.log("Your nouns are: ", definitionsNouns)
+
     return (
-        <div>
-            {props.response.word}
+        <div className='result-container'>
+            <div className='result-word'>
+                <h2>
+                    {props.response.word}
+                </h2>
+                <br />
+                /{props.response.pronunciation.all}/
+            </div>
              <br />
-                {props.response.results[0].partOfSpeech}
+            {props.response.results[0].partOfSpeech}
             <div>
-                {props.response.results[0].definition}
+                Noun 
+                <br />
+                <ul>
+                    {definitionsNouns}
+                </ul>
+                Verb 
+                <br />
+                <ul>
+                    {definitionsVerb}
+                </ul>
+                
+                {/* {props.response.results[0].definition} */}
             </div>
         </div>
     )
